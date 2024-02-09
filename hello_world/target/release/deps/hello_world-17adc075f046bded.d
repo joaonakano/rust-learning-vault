ac08f553f09a097ef6ea7bbacd317d22d4f03a59
Joao Nakano <joaovynak@gmail.com>
2024-02-09 08:43:39 -0300

    releasing with the cargo build --release

---
+++ hello_world/target/release/deps/hello_world-17adc075f046bded.d
@@ -0,0 +1,5 @@
+/home/lain/Documents/projects/hello_world/target/release/deps/hello_world-17adc075f046bded: src/main.rs
+
+/home/lain/Documents/projects/hello_world/target/release/deps/hello_world-17adc075f046bded.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/home/lain/Documents/projects/hello_world/target/release/hello_world: /home/lain/Documents/projects/hello_world/src/main.rs
